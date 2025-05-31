9c116fdc254c9452471a08b242e24542d85ae8fc
SokolovG <griso2010@yandex.ru>
2025-05-31 11:25:02 +0300

    utils tests added

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,25 @@
 from pathlib import Path
+from unittest.mock import Mock
 
 import pytest
 
 from src.core.parser import YamlParser
 from src.core.template_engine import TemplateEngine
+from src.generators.utils import (
+    StandardImportPathGenerator,
+    AdvancedImportPathGenerator,
+    FileOperations,
+)
+from src.preview.collector import PreviewCollector
 
+IMPORT_DATA = (
+    "src",
+    "domain",
+    "user_context",
+    "entities",
+    "customer",
+    "Customer",
+)
 DEFAULT_CONFIG_FILENAME = "ddd-config.yaml"
 DEFAULT_CONFIG_NOT_VALID_FILENAME = "ddd-config-not-valid.yaml"
 
@@ -49,3 +64,26 @@ def template_engine() -> TemplateEngine:
 def template_dir() -> Path:
     path = Path(__file__).parent.parent / "templates"
     return path
+
+
+@pytest.fixture
+def import_tuple() -> tuple:
+    return IMPORT_DATA
+
+
+@pytest.fixture
+def flat_import_gen() -> StandardImportPathGenerator:
+    return StandardImportPathGenerator()
+
+
+@pytest.fixture
+def nested_import_gen() -> AdvancedImportPathGenerator:
+    return AdvancedImportPathGenerator()
+
+
+@pytest.fixture
+def file_ops() -> FileOperations:
+    mock_template_engine = Mock()
+    preview_collector = PreviewCollector()
+    file_ops = FileOperations(mock_template_engine, preview_collector)
+    return file_ops
